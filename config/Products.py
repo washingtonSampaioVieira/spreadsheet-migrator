bd3d42e1c3ca3de756afd92df44735bf346ef58b
Gabriel Navevaiko <gabriel.navevaiko@primi.local>
2019-09-23 16:54:39 -0300

    - Adicionado arquivo requirements.txt - Removido atributo properties do produto

--- config/Products.py
+++ config/Products.py
@@ -5,14 +5,6 @@ from . import DatabaseField
 class ParabrisaSolicitation:
     filepath = 'C:\\Users\\gabriel.navevaiko\\Desktop\\GNV\\pára-brisa\\Solicitações\\SOLICITAÇÃO 1.xlsx'
 
-    properties = {
-        DatabaseField.ENTRY_DATE: 'Data',
-        DatabaseField.INITIAL_NUMBER: 'Nº Inicial',
-        DatabaseField.FINAL_NUMBER: 'Nº Final',
-        DatabaseField.CNPJ: 'CNPJ',
-        DatabaseField.QUANTITY: 'QTD'
-    }
-
     @property
     def file_options(self):
         return {
@@ -24,32 +16,21 @@ class ParabrisaSolicitation:
     @property
     def data_format(self):
         return {
-            DatabaseField.ENTRY_DATE: (self.properties[DatabaseField.ENTRY_DATE], DataType.DATA),
-            DatabaseField.INITIAL_NUMBER: (self.properties[DatabaseField.INITIAL_NUMBER], DataType.INT),
-            DatabaseField.FINAL_NUMBER: (self.properties[DatabaseField.FINAL_NUMBER], DataType.INT),
-            DatabaseField.CNPJ: (self.properties[DatabaseField.CNPJ], DataType.STR),
-            DatabaseField.QUANTITY: (self.properties[DatabaseField.QUANTITY], DataType.INT),
+            DatabaseField.ENTRY_DATE: ('Data', DataType.DATA),
+            DatabaseField.INITIAL_NUMBER: ('Nº Inicial', DataType.INT),
+            DatabaseField.FINAL_NUMBER: ('Nº Final', DataType.INT),
+            DatabaseField.CNPJ: ('CNPJ', DataType.STR),
+            DatabaseField.QUANTITY: ('QTD', DataType.INT),
             DatabaseField.MODEL_ID: (self.get_model_id, DataType.INT)
         }
 
-    def get_model_id(self, data, treatment_function):
+    @staticmethod
+    def get_model_id(data, treatment_function):
         return treatment_function(5)
 
 
 class ParabrisaClient:
     filepath = 'C:\\Users\\gabriel.navevaiko\\Desktop\\GNV\\pára-brisa\\Banco de clientes\\C.CLIENTES.xls'
-
-    properties = {
-        DatabaseField.CNPJ: 'CNPJ',
-        DatabaseField.NAME: 'CLIENTE',
-        DatabaseField.ADDRESS: 'ENDEREÇO',
-        DatabaseField.CITY: 'CIDADE',
-        DatabaseField.UF: 'UF',
-        DatabaseField.CEP: 'CEP',
-        DatabaseField.PHONE: ['TELEFONE1', 'TELEFONE2', 'TELEFONE3'],
-        DatabaseField.COMPANY_MANAGER: 'RESPONSAVEL',
-        DatabaseField.EMAIL: 'E-MAIL'
-    }
 
     @property
     def file_options(self):
@@ -62,21 +43,21 @@ class ParabrisaClient:
     @property
     def data_format(self):
         return {
-            DatabaseField.NAME: (self.properties[DatabaseField.NAME], DataType.STR),
-            DatabaseField.CNPJ: (self.properties[DatabaseField.CNPJ], DataType.STR),
-            DatabaseField.CITY: (self.properties[DatabaseField.CITY], DataType.STR),
-            DatabaseField.UF: (self.properties[DatabaseField.UF], DataType.STR),
-            DatabaseField.ADDRESS: (self.properties[DatabaseField.ADDRESS], DataType.STR),
-            DatabaseField.CEP: (self.properties[DatabaseField.CEP], DataType.STR),
-            DatabaseField.COMPANY_MANAGER: (self.properties[DatabaseField.COMPANY_MANAGER], DataType.STR),
+            DatabaseField.NAME: ('CLIENTE', DataType.STR),
+            DatabaseField.CNPJ: ('CNPJ', DataType.STR),
+            DatabaseField.CITY: ('CIDADE', DataType.STR),
+            DatabaseField.UF: ('UF', DataType.STR),
+            DatabaseField.ADDRESS: ('ENDEREÇO', DataType.STR),
+            DatabaseField.CEP: ('CEP', DataType.STR),
+            DatabaseField.COMPANY_MANAGER: ('RESPONSAVEL', DataType.STR),
             DatabaseField.PHONE: (self.get_phone, DataType.PHONE),
-            DatabaseField.EMAIL: (self.properties[DatabaseField.EMAIL], DataType.EMAIL)
+            DatabaseField.EMAIL: ('E-MAIL', DataType.EMAIL)
         }
 
     def get_phone(self, data, treatment_function):
         formatted_phone_list = []
 
-        for phone_key in self.properties[DatabaseField.PHONE]:
+        for phone_key in ['TELEFONE1', 'TELEFONE2', 'TELEFONE3']:
             value = data[phone_key]
             if type(value) != float:
                 formatted_phone_list.append(treatment_function(value))
